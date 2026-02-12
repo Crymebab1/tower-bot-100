3613516356c70daeed687359cab6272d443dad1d
Crymebab1 <kriskishmanlow@mail.ru>
2026-02-12 18:53:44 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,19 @@
 # ========== ТВОИ ДАННЫЕ (ВСТАВЬ СВОИ!) ==========
 
 # Токен от @BotFather
-BOT_TOKEN = "5671051022:AAHwVzL9VzNU6YNzv9nv4OUInK2jsVh1xMw"
+BOT_TOKEN = "8416278067:AAERzFY9KRbFbnvslLOlMN0_y9Uxb6CUp1k"
 
 # Telegram каналы для подписки
-TG_CHANNEL_1 = "@paris_tower_100"  # СОЗДАЙ СВОЙ КАНАЛ!
-TG_CHANNEL_2 = "@eiffel_news_100"  # СОЗДАЙ СВОЙ КАНАЛ!
-TG_LINK_1 = "https://t.me/paris_tower_100"
-TG_LINK_2 = "https://t.me/eiffel_news_100"
+TG_CHANNEL_1 = "@krismonte1"  # СОЗДАЙ СВОЙ КАНАЛ!
+TG_CHANNEL_2 = "@effnone"  # СОЗДАЙ СВОЙ КАНАЛ!
+TG_LINK_1 = "https://t.me/@krismonte1"
+TG_LINK_2 = "https://t.me/effnone"
 
 # Instagram аккаунты
-INSTAGRAM_1 = "eiffel_tower_100"    # СОЗДАЙ СВОЙ АККАУНТ!
-INSTAGRAM_2 = "paris_france_100"    # СОЗДАЙ СВОЙ АККАУНТ!
-INSTA_LINK_1 = "https://instagram.com/eiffel_tower_100"
-INSTA_LINK_2 = "https://instagram.com/paris_france_100"
+INSTAGRAM_1 = "wb_look_5000"    # СОЗДАЙ СВОЙ АККАУНТ!
+INSTAGRAM_2 = "real_krismonte"    # СОЗДАЙ СВОЙ АККАУНТ!
+INSTA_LINK_1 = "https://instagram.com/wb_look_5000"
+INSTA_LINK_2 = "https://instagram.com/real_krismonte"
 
 # База данных (заполнишь позже)
 DATABASE_URL = ""
