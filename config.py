dbb11c7b73bf836f04b8eaa3f6a317eec3fb4371
Crymebab1 <kriskishmanlow@mail.ru>
2026-02-12 18:46:05 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,22 @@
+# ========== ТВОИ ДАННЫЕ (ВСТАВЬ СВОИ!) ==========
+
+# Токен от @BotFather
+BOT_TOKEN = "5671051022:AAHwVzL9VzNU6YNzv9nv4OUInK2jsVh1xMw"
+
+# Telegram каналы для подписки
+TG_CHANNEL_1 = "@paris_tower_100"  # СОЗДАЙ СВОЙ КАНАЛ!
+TG_CHANNEL_2 = "@eiffel_news_100"  # СОЗДАЙ СВОЙ КАНАЛ!
+TG_LINK_1 = "https://t.me/paris_tower_100"
+TG_LINK_2 = "https://t.me/eiffel_news_100"
+
+# Instagram аккаунты
+INSTAGRAM_1 = "eiffel_tower_100"    # СОЗДАЙ СВОЙ АККАУНТ!
+INSTAGRAM_2 = "paris_france_100"    # СОЗДАЙ СВОЙ АККАУНТ!
+INSTA_LINK_1 = "https://instagram.com/eiffel_tower_100"
+INSTA_LINK_2 = "https://instagram.com/paris_france_100"
+
+# База данных (заполнишь позже)
+DATABASE_URL = ""
+
+# Бонусы
+BONUS_COINS = 1000
